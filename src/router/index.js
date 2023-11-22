d05c193964d103efba8b83f1f2752679741e3979
katerina-shche <asti.katerina@gmail.com>
2023-11-22 13:51:32 +0100

    finished

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: About
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id', //:str - is a rout parameter
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  //redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste placeat consectetur, consequatur delectus voluptas quisquam non quas sapiente aperiam facilis eos iusto rerum! Fuga laboriosam quo quibusdam laudantium, vel natus distinctio alias ducimus assumenda quam iure harum, adipisci rerum perspiciatis, ratione voluptas sequi omnis reiciendis architecto magni officiis. Accusamus amet ea cum qui quibusdam consequatur ipsam id! Voluptatum nobis excepturi, exercitationem ab officia consectetur facilis, beatae nisi dolorum quisquam quos, sunt totam temporibus amet eaque! Voluptate impedit beatae harum! Suscipit animi non, exercitationem quibusdam aut expedita cum sint voluptatibus? Nam a pariatur illo quod repellendus consequatur tenetur iusto vitae reprehenderit!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, dolore error expedita laboriosam consequuntur minus, sunt vitae excepturi ducimus fugiat nostrum molestiae quaerat dolor assumenda aliquid quibusdam aut, reprehenderit mollitia illo? Quos repellendus quia, cumque enim doloribus quidem impedit eos voluptas quo ipsam, placeat et nihil aperiam alias ipsum tenetur soluta quasi? Beatae id aliquam doloremque molestiae ratione vel eum officia voluptates enim ipsa aperiam, fugit, ex consectetur, possimus eaque ducimus! Est culpa tempore sint voluptates vitae praesentium iste sed ullam quae fugiat voluptatum, dolor alias! Doloribus, earum vitae? Sunt fuga, maxime corporis reprehenderit magnam autem assumenda quasi obcaecati explicabo!</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
